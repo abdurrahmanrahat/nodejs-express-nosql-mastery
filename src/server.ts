7ca358851fba5c9decfa112da22391fcd0d9b28e
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-09-13 07:02:07 +0600

    config

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,13 @@
 import mongoose from "mongoose";
 import app from "./app";
-
-const port = 5000;
+import config from "./config";
 
 async function main() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://rahat-admin:admin47@cluster0.mjja2r0.mongodb.net/foodSupply?retryWrites=true&w=majority"
-    );
+    await mongoose.connect(config.database_url as string);
 
-    app.listen(port, () => {
-      console.log(`App is listening on port ${port}`);
+    app.listen(config.port, () => {
+      console.log(`App is listening on port ${config.port}`);
     });
   } catch (error) {
     console.log("ERROR FROM MAIN SERVER", error);
