1596aa770ff62519416bb36eecd068631fa006b8
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-09-15 12:07:56 +0600

    handleDuplicateError

---
+++ src/errors/handleDuplicateError.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { TErrorSources } from "../interface/error";
+
+export const handleDuplicateError = (err: any) => {
+  const statusCode = 400;
+
+  const match = err.errmsg.match(/"([^"]*)"/);
+  const extractedMessage = match && match[1];
+
+  const errorSources: TErrorSources = [
+    {
+      path: "",
+      message: `${extractedMessage} is already exists.`,
+    },
+  ];
+
+  return {
+    statusCode,
+    message: "Invalid Id",
+    errorSources,
+  };
+};
--- src/middleware/globalErrHandler.ts
+++ src/middleware/globalErrHandler.ts
@@ -2,6 +2,7 @@
 /* eslint-disable no-unused-vars */
 import { ErrorRequestHandler } from "express";
 import { handleCastError } from "../errors/handleCastError";
+import { handleDuplicateError } from "../errors/handleDuplicateError";
 import { handleValidationError } from "../errors/handleValidationError";
 import { TErrorSources } from "../interface/error";
 
@@ -23,6 +24,12 @@ export const globalErrHandler: ErrorRequestHandler = (err, req, res, next) => {
     errorSources = simplifiedError.errorSources;
   } else if (err.name === "CastError") {
     const simplifiedError = handleCastError(err);
+
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSources = simplifiedError.errorSources;
+  } else if (err.code === 11000) {
+    const simplifiedError = handleDuplicateError(err);
 
     statusCode = simplifiedError.statusCode;
     message = simplifiedError.message;
