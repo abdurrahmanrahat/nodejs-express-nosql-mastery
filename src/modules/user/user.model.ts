afa3a4275742e1d798d7548b82c48be4a8fa7531
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2025-04-20 15:29:09 +0600

    mongoose hook

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -1,4 +1,6 @@
+import bcrypt from "bcryptjs";
 import { model, Schema } from "mongoose";
+import config from "../../config";
 import { USER_Role, USER_Status } from "./user.constants";
 import { TUser } from "./user.interface";
 
@@ -31,6 +33,18 @@ const userSchema = new Schema<TUser>({
     type: Date,
     default: Date.now,
   },
+});
+
+userSchema.pre("save", async function (next) {
+  this.password = await bcrypt.hash(this.password, Number(config.salt_round));
+
+  next();
+});
+
+userSchema.post("save", function (doc, next) {
+  doc.password = "";
+
+  next();
 });
 
 export const User = model<TUser>("User", userSchema);
